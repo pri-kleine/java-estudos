ce4587565d9801162ad96ef340ff98b5bd16515f
Priscilla K <priscillakleine@gmail.com>
2024-01-19 22:02:01 -0300

    conceitos inicias de SOLID-SRP

---
+++ POOII/src/main/java/br/com/ada/pooii/SOLID/exercicioSRP/Pedido.java
@@ -0,0 +1,31 @@
+package POOII.src.main.java.br.com.ada.pooii.SOLID.exercicioSRP;
+
+import java.util.List;
+
+class Pedido {
+    private long id;
+    private List<Item> itens;
+
+    // getters e setters
+}
+
+class Item {
+    // Detalhes do item
+}
+
+class Imprimicao {
+    public void imprimirPedido() { }
+    public void imprimirItens() { }
+}
+
+class StatusPedido {
+    public void buscarPedido(long id) { /* Violação do SRP */ }
+    public void salvar(Pedido pedido) { /* Violação do SRP */ }
+    public void atualizar(long id, Pedido pedido) { /* Violação do SRP */ }
+    public void deletar(long id) { /* Violação do SRP */ }
+}
+
+class EmailService {
+    public void enviarEmailConfirmacao(String mensagem) { /* Violação do SRP */ }
+    public void enviarParaTransporte() { /* Violação do SRP */ }
+}
